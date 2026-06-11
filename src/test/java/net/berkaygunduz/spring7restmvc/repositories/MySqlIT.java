925ec526ef4b9c6e720572000b21435155f12008
erolberkaygunduz <erolberkaygunduz@gmail.com>
2026-06-11 23:58:23 +0200

    v1.4.5
    Test Container and Docker

---
+++ src/test/java/net/berkaygunduz/spring7restmvc/repositories/MySqlIT.java
@@ -0,0 +1,38 @@
+package net.berkaygunduz.spring7restmvc.repositories;
+
+import net.berkaygunduz.spring7restmvc.entity.Beer;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.*;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("localmysql")
+public class MySqlIT {
+
+    @Container
+    @ServiceConnection
+    static MySQLContainer<?> mySQLContainer = new MySQLContainer("mysql:9");
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeer(){
+        List<Beer> beers = beerRepository.findAll();
+
+        assertThat(beers.size()).isGreaterThan(0);
+    }
+
+
+}
